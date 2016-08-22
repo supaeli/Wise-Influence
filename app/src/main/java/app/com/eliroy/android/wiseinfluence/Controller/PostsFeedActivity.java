87e6520cddd69fff38c934609e5438b7e821e6f5
supeeli <elay17@gmail.com>
2016-08-22 21:48:11 +0300

    create simple test app with list adapter - list item is just a text view

--- app/src/main/java/app/com/eliroy/android/wiseinfluence/Controller/PostsFeedActivity.java
+++ app/src/main/java/app/com/eliroy/android/wiseinfluence/Controller/PostsFeedActivity.java
@@ -2,14 +2,22 @@ package app.com.eliroy.android.wiseinfluence.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import app.com.eliroy.android.wiseinfluence.R;
 
 public class PostsFeedActivity extends AppCompatActivity {
 
+    private String[] items = {"Post1", "Post2", "Post3", "Post4", "Post5", "Post6"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts_feed);
+
+        ListView myList = (ListView)findViewById(R.id.feed_list_view);
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this,R.layout.simple_post_item_template, items);
+        myList.setAdapter(myArrayAdapter);
     }
 }
