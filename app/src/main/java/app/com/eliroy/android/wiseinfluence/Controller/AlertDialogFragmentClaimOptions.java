67cec350e196ef40816a7b27ac49f3d5b85f6297
supeeli <elay17@gmail.com>
2016-09-23 17:15:30 +0300

    Refactoring

--- app/src/main/java/app/com/eliroy/android/wiseinfluence/Controller/AlertDialogFragmentClaimOptions.java
+++ app/src/main/java/app/com/eliroy/android/wiseinfluence/Controller/AlertDialogFragmentClaimOptions.java
@@ -25,60 +25,64 @@ public class AlertDialogFragmentClaimOptions extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         String[] claimOptions = getResources().getStringArray(R.array.claim_options);
-       final PostDetailsActivity parent = (PostDetailsActivity) getActivity();
 
         return new AlertDialog.Builder(getActivity())
                 .setItems(claimOptions, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        PostDetailsActivity parent = (PostDetailsActivity) getActivity();
-                        String email = parent.politicians.size() > 0 ? parent
-                                .politicians.get(0).getEmail() : "";
-                        String topic = parent.post.getTopic();
-                        String templateContent = "";
-                        //find and assign template content
-                        for (int templatesIndex = 0;
-                             templatesIndex < parent.templates.size(); templatesIndex++){
-
-                                 if(parent.templates.get(templatesIndex).getCategory()
-                                        .equals(parent.post.getParentCategory())){
-                                     templateContent = parent.templates.get(templatesIndex).getContent();
-                                 }
-
-                        }
 
                         switch (i){
                             case 0: {
-                                Intent intent = new Intent(Intent.ACTION_SENDTO);
-                                String uriText = "mailto:" + Uri.encode(email) +
-                                        "?subject=" + Uri.encode(topic) +
-                                        "&body=" + Uri.encode(templateContent);
-                                Uri uri = Uri.parse(uriText);
-                                intent.setData(uri);
-                                startActivity(Intent.createChooser(intent,"send mail"));
-
-                                try {
-                                    startActivity(Intent.createChooser(intent,"send mail"));
-                                } catch(ActivityNotFoundException e){
-                                    e.printStackTrace();
-                                    Log.i("INFO", "No email client found");
-                                    Toast.makeText(parent.getApplicationContext(),
-                                            "No email app found, lease install and try again",
-                                            Toast.LENGTH_LONG)
-                                            .show();
-                                }
+                                showEmailClaim();
                             }
                             case 1:{
                                 // TODO: 09/09/2016 call facebook intent creator
+                               // showFacebookClaim();
                             }
                             case 2:{
                                 // TODO: 09/09/2016 call phone call intent creator
+                                //showPhoneCallClaim();
                             }
                         }
-
                     }
                 })
                 .create();
+    }
+
+    private void showEmailClaim() {
+        PostDetailsActivity parent = (PostDetailsActivity) getActivity();
+        String email = parent.politicians.size() > 0 ? parent.politicians.get(0).getEmail() : "";
+        String topic = parent.post.getTopic();
+
+        String templateContent = "";
+        //find and assign template content
+        for (int templatesIndex = 0;
+             templatesIndex < parent.templates.size(); templatesIndex++){
+
+            if(parent.templates.get(templatesIndex).getCategory()
+                    .equals(parent.post.getParentCategory())){
+                templateContent = parent.templates.get(templatesIndex).getContent();
+            }
+        }
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        String uriText = "mailto:" + Uri.encode(email) +
+                "?subject=" + Uri.encode(topic) +
+                "&body=" + Uri.encode(templateContent);
+        Uri uri = Uri.parse(uriText);
+        emailIntent.setData(uri);
+        startActivity(Intent.createChooser(emailIntent,"send mail"));
+
+        try {
+            startActivity(Intent.createChooser(emailIntent,"send mail"));
+        } catch(ActivityNotFoundException e){
+            e.printStackTrace();
+            Log.i("INFO", "No email client found");
+            Toast.makeText(parent.getApplicationContext(),
+                    "No email app found, lease install and try again",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
 }
